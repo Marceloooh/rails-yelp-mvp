49c6b518245d69e683cc3267b5b3b401fce513d6
MK <publicelo@yahoo.com.au>
2022-07-23 10:20:24 +0200

    Yelp almost finish...

---
+++ db/migrate/20220721182026_change_object_to_restaurants.rb
@@ -0,0 +1,6 @@
+class ChangeObjectToRestaurants < ActiveRecord::Migration[6.1]
+  def change
+    change_column :restaurants, :phone_number, :string
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database"
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+chez_la_vielle = { name: "Chez la vieille", address: "Rue de Gand, Lille", phone_number: "0245243300", category: "french" }
+levv = { name: "Le verre volé", address: "Rue de Lancry, Paris", phone_number: "0323490056", category: "french" }
+becane = { name: "La Bécane à Gaston", address: "Rue Lucien Sampaix", phone_number: "0746532219", category: "french" }
+felicita = { name: "La Félicita", address: "Station F", phone_number: "0246443300", category: "italian" }
+ravioli = { name: "Ravioli Nord Est", address: "Rue Civiale", phone_number: "0648657894", category: "chinese" }
+
+[chez_la_vielle, levv, becane, felicita, ravioli].each do |resto|
+  new = Restaurant.create!(resto)
+  puts "The restaurant #{new.name} is created"
+end
+puts "Finished!"
